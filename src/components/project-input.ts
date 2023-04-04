6a1391123b6a925250fe224bf821eaa1307e1304
Viktoryia Varatnitskaya <vorotnic@Viktoryias-MacBook-Air.local>
2023-04-03 21:50:14 -0700

    class 0403-2

---
+++ src/components/project-input.ts
@@ -0,0 +1,87 @@
+/// <reference path="base-component.ts"/>
+/// <reference path="../decorators/autobind.ts"/>
+/// <reference path="../util/validation.ts"/>
+/// <reference path="../state/project-state.ts"/>
+
+namespace App {
+  //Project input class
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    poeopleInputElement: HTMLInputElement;
+
+    constructor() {
+      super("project-input", "app", true, "user-input");
+      this.titleInputElement = this.element.querySelector(
+        "#title"
+      )! as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector(
+        "#description"
+      )! as HTMLInputElement;
+      this.poeopleInputElement = this.element.querySelector(
+        "#people"
+      )! as HTMLInputElement;
+
+      this.configure();
+    }
+
+    configure() {
+      this.element.addEventListener("submit", this.submitHandler.bind(this));
+    }
+
+    renderContent() {}
+
+    private gatherUserInput(): [string, string, number] | void {
+      const enteredTitle = this.titleInputElement.value;
+      const enteredDescription = this.descriptionInputElement.value;
+      const enteredPeople = this.poeopleInputElement.value;
+
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+      };
+
+      const descriptionValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+        minLength: 5,
+      };
+
+      const peopleValidatable: Validatable = {
+        value: enteredPeople,
+        required: true,
+        min: 1,
+        max: 5,
+      };
+
+      if (
+        !validate(titleValidatable) ||
+        !validate(descriptionValidatable) ||
+        !validate(peopleValidatable)
+      ) {
+        alert("Invlid input. Please try again");
+        return;
+      } else {
+        return [enteredTitle, enteredDescription, +enteredPeople];
+      }
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      const userInput = this.gatherUserInput();
+      if (Array.isArray(userInput)) {
+        const [title, desc, people] = userInput;
+        projectState.addProject(title, desc, people);
+        console.log(title, desc, people);
+        this.clearInputs();
+      }
+    }
+
+    private clearInputs() {
+      this.titleInputElement.value = "";
+      this.descriptionInputElement.value = "";
+      this.poeopleInputElement.value = "";
+    }
+  }
+}
