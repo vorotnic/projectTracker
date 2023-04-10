296d264d280ce3c2aef21c3f59eefd99cf4d114f
Viktoryia Varatnitskaya <vorotnic@Viktoryias-MacBook-Air.local>
2023-04-09 18:54:17 -0700

    webpack config adjustmetns

---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin')
+ 
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-cheap-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+new CleanPlugin.CleanWebpackPlugin()
+  ]
+};
